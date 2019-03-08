c4bdaff730e4c06d9bdb8320663d13e5c1b47184
Leonardo <leonardooc@globo.com>
2019-03-08 00:00:14 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,41 @@
-var dados = []
+var dados = [];
+var pipefile = [];
 
 function setup() {
     createCanvas(800, 600);
     button = createButton('Rolar dado');
-    button.position(1, height-100);
-    button.mousePressed(rolarDado);
-    dado = new Dado();
-    dado2 = new Dado();
-    dados.push(dado);
-    dados.push(dado2);
+    button.position(1, 1);
+    button.mouseClicked(rolarDado);
 }
 
 function draw() {
-    dado.show();
-    dado2.show();
+    if(dados.length>0){
+       for(let i=0;i<dados.length;i++){
+           dados[i].show();
+       }
+     
+    }
 }
 
 function mousePressed(){
-    dado.shuffle();
-    dado2.shuffle();
+//    dado.shuffle();
+//    dado2.shuffle();
 }
 
 function rolarDado(){
-    for(let i=0;i<=dados.length;i++){
-        dados[i].shuffle();
+    if(dados.length>0){
+        for(let i=0;i<dados.length;i++){
+            dados[i].shuffle();
+        }
+    }
+}
+
+function limpaDados(){
+    dados = [];
+}
+
+function criaDados(num){
+    for(let i=0;i<num;i++){
+        dados.push(new Dado());
     }
 }
