437d35e027c036de4651b2b67451f016e017e935
Leonardo <leonardooc@globo.com>
2019-03-12 08:12:32 -0300

    Sketch com combate em 2 funcionando com namespace

--- sketch.js
+++ sketch.js
@@ -1,49 +1,128 @@
-var dados = [];
-
-function setup() {
-    var canvas = createCanvas(640, 480);
-    canvas.parent('area-sketch');
+var sketch1 = function(p){
     
-    button = createButton('Rolar dado');
-    button.position(width, height);
-    button.parent('area-sketch');
-    button.mouseClicked(rolaDado);
-}
+    var dados = [];
+    
+    p.setup = function(){
+        var canvas = p.createCanvas(640, 480);
+        canvas.parent('area-sketch');
+    
+        p.button = p.createButton('Rolar dado');
+        p.button.position(p.width, p.height);
+        p.button.parent('area-sketch');
+        p.button.mouseClicked(p.rolaDado);
+    };
+    
+    p.draw = function(){
+        if(dados.length>0){
+            for(var i=0;i<dados.length;i++){
+                dados[i].show();
+            }
+        }
+    };
+    
+    p.rolaDado = function(){
+        var total=0;
+        if(dados.length>0){
+            for(var i=0;i<dados.length;i++){
+                dados[i].shuffle();
+                total+=dados[i].getValor();
+            }
 
-function draw() {
-    if(dados.length>0){
-       for(let i=0;i<dados.length;i++){
-           dados[i].show();
-       }
-     
-    }
-}
+            return(total);
+        }
+    };
+    
+    p.limpaDados = function(){
+        dados = [];
+    };
+    
+    p.criaDados = function(num){
+        for(var i=0;i<num;i++){
+            dados.push(new Dado());
+        }
+    };
+    
+    p.limpaTela = function(){
+        p.clear();    
+    };
 
-function mousePressed(){
-}
+    function Dado(){   
+    
+    this.lado = 80;
+    this.x = p.random(1,p.width-this.lado);
+    this.y = p.random(1,p.height-this.lado);
 
-function rolaDado(){
-    let total=0;
-    if(dados.length>0){
-        for(let i=0;i<dados.length;i++){
-            dados[i].shuffle();
-            total+=dados[i].getValor();
+    this.type = 6;
+    
+    this.show = function(){
+        p.stroke(0,0,0);
+        p.fill(255,255,255);
+        
+        p.rect(this.x,this.y, this.lado, this.lado);
+
+        switch(this.type){
+            case 1:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.5, this.y+this.lado*0.5, 10, 10);
+                break;
+            case 2:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                break;
+            case 3:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.5, this.y+this.lado*0.5, 10, 10);
+                break;
+            case 4:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                break;
+            case 5:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.5, this.y+this.lado*0.5, 10, 10);
+                break;
+            case 6:
+                p.fill(0,0,0);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, this.y+this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, (this.y+this.lado)-this.lado*0.25, 10, 10);
+                p.ellipse(this.x+this.lado*0.25, this.y+this.lado*0.5, 10, 10);
+                p.ellipse((this.x+this.lado)-this.lado*0.25, this.y+this.lado*0.5, 10, 10);
+                break;
         }
 
-        return(total);
+    }
+
+    this.clear = function(){
+
+    }
+
+    this.shuffle = function(){
+        this.type = p.ceil(p.random(0,6));
+    }
+    
+    this.shufflePos = function(){
+        times = 100;
+        speed = 50;
+        this.xdirection = p.random(-1,1);
+        this.ydirection = p.random(-1,1);
+    }
+
+    this.getValor = function(){
+        return(this.type);
     }
 }
 
-function limpaDados(){
-    dados = [];
-}
-
-function criaDados(num){
-    for(let i=0;i<num;i++){
-        dados.push(new Dado());
-    }
-}
-
-function limpaTela(){
-    clear();
-}
+};
+var mesa_dados = new p5(sketch1);
