bb0c34a3d9fbd24594815df9a991b0066c129af9
Leonardo <leonardooc@globo.com>
2019-03-12 23:24:42 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -125,4 +125,48 @@ var sketch1 = function(p){
 }
 
 };
+
+var sketch2 = function(p){
+    p.setup = function(){
+        var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+        canvas.class("tela-efeitos");
+        p.x = 1;
+        p.y = 1;
+        p.hit_sword = false;
+    };
+    
+    p.draw = function(){
+        
+        if(p.hit_sword){
+            p.strokeWeight(6);
+            p.stroke(210,0,0);
+            
+            if(p.x<=200){
+                p.background(255);
+            }
+            else{
+                p.clear();
+            }
+            
+            p.line(50,50,p.x,p.y);
+            p.x+=90;
+            p.y+=45;
+            
+            if(p.x>=p.windowWidth){
+                p.hit_sword=false; 
+                p.x=p.y=1;
+                p.clear();
+            }
+        }
+        
+        
+        
+    };
+    
+    p.hit = function(type){
+        p.hit_sword = true;
+    };
+};
+
 var mesa_dados = new p5(sketch1);
+var tela_efeitos = new p5(sketch2);
